2a3701b6e9d2bfceb7f3a9b5e3a932da4f697f4a
AriRVasc <ariadnerv17@gmail.com>
2022-05-30 22:42:49 -0300

    App de Cadastro utilizando Spring Boot

---
+++ src/main/resources/db/migration/V2__Populate_Table_Pessoas.sql
@@ -0,0 +1,5 @@
+INSERT INTO `tb_pessoas` VALUES (1,'flavia@digitalhouse.com','Flavia','Flavinha','Almeida'),
+(2,'filhosalmeidaalexandre@gmail.com','Julia','Juju','Almeida'),
+(3,'davia@teste.com','Davi','Davizinho','Almeida'),
+(4,'jaugusto@teste.com','Joao Augusto','Jaugusto','Almeida'),
+(5,'fabiane@gmail.com','Fabiane','Fabi','Almeida');
